63e2e56e1415d610e30ca2691b851e2b75fc3111
Shaun <swarman@paypal.com>
2020-03-29 16:44:02 -0500

    feat: add ability for admin to remove users 2fa

--- routes/web/2fa.js
+++ routes/web/2fa.js
@@ -10,10 +10,10 @@ router.use(policies.ensureLoggedIn);
 router
   .get('/otp/login', render('2fa/otp-login'))
   .post('/otp/login', web.auth.loginOtp)
-  .post('/recovery', web.auth.recovery)
+  .post('/recovery', web.twoFactor.recovery.recover)
   .get('/recovery/verify', render('2fa/verify'))
-  .post('/recovery/verify', web.auth.recoveryVerify)
-  .get('/recovery-keys', render('2fa/otp-recovery'))
-  .post('/recovery-keys', web.auth.recoveryKey);
+  .post('/recovery/verify', web.twoFactor.recovery.verify)
+  .get('/recovery/keys', render('2fa/otp-recovery'))
+  .post('/recovery/keys', web.auth.recoveryKey);
 
 module.exports = router;
